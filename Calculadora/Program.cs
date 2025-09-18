afc5767d73ef7bbaa15401e01d5aa1717fe81cbe
CostaFelip <costaffelipe@icloud.com>
2025-09-18 14:47:31 -0300

    v1.0

---
+++ Calculadora/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CALCULADORA");
+            Console.WriteLine("Versão 1.0 - Estrutura inicial");
+
+            Console.WriteLine("Pressione qualquer tecla para sair...");
+            Console.ReadKey();
+        }
+    }
+}
