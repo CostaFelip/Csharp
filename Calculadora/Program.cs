fb30a20e490b42939fb13e7379243670f06f671b
CostaFelip <costaffelipe@icloud.com>
2025-09-18 14:52:56 -0300

    v1.1

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,16 +1,62 @@
 ﻿using System;
 
-namespace Calculadora
+namespace CalculadoraBasica
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("CALCULADORA");
-            Console.WriteLine("Versão 1.0 - Estrutura inicial");
-
-            Console.WriteLine("Pressione qualquer tecla para sair...");
-            Console.ReadKey();
+            
+            bool continuar = true;
+            
+            while (continuar)
+            {
+                ExibirMenu();
+                
+                Console.Write("Escolha uma opção: ");
+                string opcao = Console.ReadLine();
+                
+                switch (opcao)
+                {
+                    case "1":
+                        Console.WriteLine("Soma selecionada");
+                        break;
+                    case "2":
+                        Console.WriteLine("Subtração selecionada");
+                        break;
+                    case "3":
+                        Console.WriteLine("Multiplicação selecionada");
+                        break;
+                    case "4":
+                        Console.WriteLine("Divisão selecionada");
+                        break;
+                    case "5":
+                        continuar = false;
+                        Console.WriteLine("Saindo...");
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida!");
+                        break;
+                }
+                
+                if (continuar)
+                {
+                    Console.WriteLine("\nPressione qualquer tecla para continuar...");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
+        }
+        
+        static void ExibirMenu()
+        {
+            Console.WriteLine("\n=== MENU ===");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão");
+            Console.WriteLine("5 - Sair");
         }
     }
 }
